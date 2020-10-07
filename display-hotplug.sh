88f4c06281274b97362754f1001e59cdcb374976
zemiret <dev@amleczko.pl>
2020-10-07 23:24:15 +0200

    remove nohup out

--- display-hotplug.sh
+++ display-hotplug.sh
@@ -7,7 +7,7 @@ user=zemiret
 userid=$(id $user | grep -o -e 'uid=[0-9]*' | grep -o -e '[0-9]*')
 
 if [ $HDMI_STATUS == "connected" ]; then
-	nohup sleep 1 && xrandr --output HDMI2 --mode 1920x1200 --output eDP1 --mode 1280x720 --right-of HDMI2 &
+	nohup sleep 1 > /dev/null 2>&1 && xrandr --output HDMI2 --mode 1920x1200 --output eDP1 --mode 1280x720 --right-of HDMI2 &
 	sudo -u $user DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$userid/bus /usr/bin/notify-send --urgency=low -t 5000 "Damn visuals!" "HDMI and display connected"
 elif [ $DP1_STATUS == "connected" ]; then
 	xrandr --output HDMI2 --off --output eDP1 --mode 1920x1080
