846e6e92e398a0ebcaa1dba57047afb544c0fc9f
zemiret <dev@amleczko.pl>
2020-10-07 12:46:04 +0200

    display hotplug

---
+++ display-hotplug.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DP1_STATUS=$(</sys/class/drm/card0/card0-eDP-1/status )
+HDMI_STATUS=$(</sys/class/drm/card0/card0-HDMI-A-2/status)
+
+user=zemiret
+userid=$(id $user | grep -o -e 'uid=[0-9]*' | grep -o -e '[0-9]*')
+
+if [ $HDMI_STATUS == "connected" ]; then
+	nohup sleep 1 && xrandr --output HDMI2 --mode 1920x1200 --output eDP1 --mode 1280x720 --right-of HDMI2 &
+	sudo -u $user DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$userid/bus /usr/bin/notify-send --urgency=low -t 5000 "Damn visuals!" "HDMI and display connected"
+elif [ $DP1_STATUS == "connected" ]; then
+	xrandr --output HDMI2 --off --output eDP1 --mode 1920x1080
+	sudo -u $user DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$userid/bus /usr/bin/notify-send --urgency=low -t 5000 "Damn visuals!" "Only laptop"
+else
+	xrandr --auto
+fi
+
